d53e11438f0caa9a5d11ac473aa00a146ddfdad2
Steve Sanderson <steve@codeville.net>
2014-03-27 23:20:00 +0000

    Publish all HTML files

--- gulpfile.js
+++ gulpfile.js
@@ -50,7 +50,7 @@ gulp.task('css', function () {
 });
 
 gulp.task('html', function() {
-    return gulp.src('./src/index.html')
+    return gulp.src('./src/*.html')
         .pipe(htmlreplace({
             'css': 'css.css',
             'js': 'scripts.js'
